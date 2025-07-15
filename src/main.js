bef5faf86596473eb0ba79ec23cf93b1c7870ebd
doves5695 <790150796@qq.com>
2025-07-15 20:07:00 +0800

    更新form

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import { createPinia } from 'pinia';
 import './style.css';
 import router from './router';
 import App from './App.vue';
+import ElementPlus from 'element-plus';
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount('#app');
+createApp(App).use(router).use(pinia).use(ElementPlus).mount('#app');
